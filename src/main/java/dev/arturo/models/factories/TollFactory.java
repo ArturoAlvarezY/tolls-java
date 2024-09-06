2400aaeb9dabf503162fed3f46298ff7127d93cd
Arturo Alvarez <153916290+ArturoAlvarezY@users.noreply.github.com>
2024-09-06 17:54:52 +0200

    adding atributes and methods to my Abstract class TollFactory, must add these methods in its children

--- src/main/java/dev/arturo/models/factories/TollFactory.java
+++ src/main/java/dev/arturo/models/factories/TollFactory.java
@@ -1,7 +1,37 @@
 package dev.arturo.models.factories;
 
+import dev.arturo.models.implementation.IVehicle;
+
 public abstract class TollFactory {
    
+    private String name;
+    private String city;
+    private Number tollAmount;
 
+    public TollFactory(String name, String city, Number tollAmount) {
+        this.name = name;
+        this.city = city;
+        this.tollAmount = tollAmount;
+    }
+
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getCity() {
+        return city;
+    }
+    public void setCity(String city) {
+        this.city = city;
+    }
+    public Number getTollAmount() {
+        return tollAmount;
+    }
+    public void setTollAmount(Number tollAmount) {
+        this.tollAmount = tollAmount;
+    }
     
+    public abstract IVehicle amountToPay();
 }   
