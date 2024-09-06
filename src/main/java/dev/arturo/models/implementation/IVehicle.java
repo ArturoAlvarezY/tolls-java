9acaa8e158ea38da09fdc35ca2004bbac411f9be
Arturo Alvarez <153916290+ArturoAlvarezY@users.noreply.github.com>
2024-09-06 18:23:35 +0200

    creating the interface and implementhing this to the products class

--- src/main/java/dev/arturo/models/implementation/IVehicle.java
+++ src/main/java/dev/arturo/models/implementation/IVehicle.java
@@ -1,5 +1,7 @@
 package dev.arturo.models.implementation;
 
+import dev.arturo.models.factories.TollFactory;
+
 public interface IVehicle {
-    
+    public TollFactory amountToPay();
 }
--- src/main/java/dev/arturo/models/vehiclesproduct/Motocycle.java
+++ src/main/java/dev/arturo/models/vehiclesproduct/Motocycle.java
@@ -1,7 +1,13 @@
 package dev.arturo.models.vehiclesproduct;
 
+import dev.arturo.models.factories.TollFactory;
 import dev.arturo.models.implementation.IVehicle;
 
 public class Motocycle implements IVehicle {
+
+    @Override
+    public TollFactory amountToPay() {
+        throw new UnsupportedOperationException("Unimplemented method 'amountToPay'");
+    }
     
 }
