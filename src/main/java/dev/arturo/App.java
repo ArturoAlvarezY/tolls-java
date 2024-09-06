190aa02b867231eabcba66f8c1661cfa43821f1a
Arturo Alvarez <153916290+ArturoAlvarezY@users.noreply.github.com>
2024-09-06 23:03:57 +0200

    add the main method, finished exercise

--- src/main/java/dev/arturo/App.java
+++ src/main/java/dev/arturo/App.java
@@ -1,5 +1,10 @@
 package dev.arturo;
 
+import dev.arturo.models.Toll;
+import dev.arturo.models.factories.TruckFactory;
+import dev.arturo.models.factories.VehicleFactory;
+import dev.arturo.models.implementation.IVehicle;
+
 /**
  * Hello world!
  */
@@ -12,6 +17,18 @@ public final class App {
      * @param args The arguments of the program.
      */
     public static void main(String[] args) {
-        System.out.println("Hello World!");
-    }
-}
+        
+Toll toll = new Toll("Central Toll", "Cityville", 0, null);
+
+        String truckRegistration = "ABC123";
+        int truckAxis = 4;
+
+        VehicleFactory truckFactory = new TruckFactory(truckRegistration, truckAxis);
+        IVehicle truck = truckFactory.creatVehicle();
+
+        System.out.println("Truck with registration " + truckRegistration + " has " + truckAxis + " axis.");
+        Integer amountToPay = truck.amountToPay(toll);
+        System.out.println("Amount to pay for the truck: $" + amountToPay);
+        System.out.println("Updated toll amount: $" + toll.getTollAmount());
+    }    }
+
